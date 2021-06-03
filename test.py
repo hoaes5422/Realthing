4ecf8448b61d621f20260d6b01e62442dad255d5
Hoaes5422 <dang3638@gmail.com>
2021-06-03 09:56:23 +0700

    Update test.py

--- test.py
+++ test.py
@@ -11,3 +11,17 @@ print(f"the name have {len(name)} elements")
 
 for num in [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]:
 	print(num)
+
+
+
+for x in range(105):
+	print(x)
+
+names = ["harry","potter","witch","hayla"]
+for x in names:
+	print(x)
+
+name = "Hubert Blaine Wolfeschlegelsteinhausenbergerdorff"
+
+for y in name:
+	print(y)
