38fb6d009c3c20e5a47ab89e8551bdc8efcccc26
Hoaes5422 <dang3638@gmail.com>
2021-06-04 11:03:49 +0700

    Create okna.py

---
+++ now/okna.py
@@ -0,0 +1,19 @@
+class students:
+
+	def __init__(self, name, major, gpa):
+		self.name = name
+		self.major = major
+		self.gpa = gpa 
+
+class question:
+	def __init__(self, question,answer):
+		self.question = question
+		self.answer = answer
+
+
+
+
+
+
+
+
