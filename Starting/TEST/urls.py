d3d468c83b1de5feedc532c19257349eace444a5
Hoaes5422 <dang3638@gmail.com>
2021-06-04 17:16:11 +0700

    Update urls.py

--- Starting/TEST/urls.py
+++ Starting/TEST/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
 	path("",views.index , name="index"),
-	path("<str:name>",views.greet, name="greet")
+	# path("<str:name>",views.greet, name="greet"),
+	path("h1",views.seperate, name="seperate"),
+	path("h3.css",views.hay, name="css")
 
 ]
