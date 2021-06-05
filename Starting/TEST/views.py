f6dd6ad9333dca71d0c15ac0be370f7fe4baaac0
Hoaes5422 <dang3638@gmail.com>
2021-06-05 10:37:35 +0700

    Update views.py

--- Starting/TEST/views.py
+++ Starting/TEST/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-	return HttpResponse("hello world!")
-
-# def greet(request, name):
-# 	return HttpResponse(f"hello, {name}")
-
-
-def seperate(request):
 	return render(request, "table.html")
 
-def hay(request):
-	return render(request,"h3.css")
+def greet(request, name):
+	return render(request, "greet.html", {
+		"name": name.capitalize()
+		})
+
+
+
 
