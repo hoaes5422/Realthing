a351a5c8f2fcba389c93852264719bac2c411ea1
Hoaes5422 <dang3638@gmail.com>
2021-06-05 10:37:16 +0700

    Update urls.py

--- Starting/Starting/urls.py
+++ Starting/Starting/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('TEST/', include("TEST.urls"))
+    path('TEST/', include("TEST.urls")),
+    path('Newyear/', include("Newyear.urls"))
     
 ]
