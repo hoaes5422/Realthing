028528e78ca40b6b0e6cdf6bb5d88a54702d3a14
Hoaes5422 <dang3638@gmail.com>
2021-06-06 08:51:04 +0700

    Update views.py

--- Starting/Task/views.py
+++ Starting/Task/views.py
@@ -1,15 +1,37 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
-tasks = ["Check the homework",
-"Write down the idea ",
-"End the thing you start"]
+class Newtask(forms.Form):
+	task = forms.CharField(label="New task")
+	# priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 def list(request):
+	if "tasks" not in request.session:
+		request.session["tasks"] = []
 	return render(request,"list.html",{
 
-		"tasks": tasks
+		"tasks": request.session["tasks"]
 		})
 
 def add(request):
-	return render(request,"add.html")
+	if request.method == "POST":
+		form = Newtask(request.POST)
+		if form.is_valid():
+			task = form.cleaned_data["task"]
+			if task == "Delete":
+				request.session["tasks"] = []
+				return HttpResponseRedirect(reverse("tasks:list"))
+			else:
+				request.session["tasks"] += [task]
+				return HttpResponseRedirect(reverse("tasks:list"))
+		else:
+			return render(request, "add.html",{
+				"form": form 
+				})
+	return render(request,"add.html",{
+		"form":  Newtask()
+
+		})
