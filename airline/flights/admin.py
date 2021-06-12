a4978bff03772d5c189b1effb1c516bbad8f6651
Hoaes5422 <dang3638@gmail.com>
2021-06-12 20:29:46 +0700

    Update admin.py

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Airport, Flight, Passenger
 # Register your models here.
-admin.site.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+	list_display = ("id", "origin", "destination", "duration")
+
+class PassengerAdmin(admin.ModelAdmin):
+	filter_horizontal = ("flights",)
+
+admin.site.register(Flight, FlightAdmin)
 admin.site.register(Airport)
-admin.site.register(Passenger)
+admin.site.register(Passenger, PassengerAdmin)
