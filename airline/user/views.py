9b1043c2fad91af24a4ed52d64492fb14a555530
Hoaes5422 <dang3638@gmail.com>
2021-06-13 09:20:19 +0700

    Update views.py

--- airline/user/views.py
+++ airline/user/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth import  authenticate, login, logout
 # Create your views here.
 def index(request):
 	if not request.user.is_authenticated:
-		return HttpResponseRedirect(reverse('login'))
+		return HttpResponseRedirect(reverse("login"))
+	return render(request, "user.html")
 
 
 def login_view(request):
+	if request.method == "POST":
+		username = request.POST["username"]
+		password = request.POST["password"]
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return HttpResponseRedirect(reverse("index"))
+		else:
+			return render(request,"login.html", {
+				"message": "Invalid"
+				})
 	return render(request,"login.html")
 
 
 def logout_view(request):
-	return render(request,"logout.html")
+	logout(request)
+	return render(request,"logout.html", {
+		"message": "Logged out."
+		})
